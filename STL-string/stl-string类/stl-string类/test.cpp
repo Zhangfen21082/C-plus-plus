5c7fe6304e46ee40efffdafed7ff537fc6706376
Zhangfen21082 <290440677@qq.com>
2021-03-22 14:19:16 +0800

    string类

---
+++ STL-string/stl-string类/stl-string类/test.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//int main()
+//{
+//	string url("http://www.cplusplus.com/reference/string/string/substr/");//原网址
+//	size_t pos1 = url.find(":");//pos1用来定位冒号
+//
+//	size_t pos2 = pos1 + 3;//pos2进行偏移，偏移值www开头
+//	size_t pos3 = url.find(".com/");//pos用来找到域名结束
+//	pos3 += 4;//偏移至资源开始处
+//
+//
+//	string first = url.substr(0, 4);
+//	string second = url.substr(pos2, pos3 - pos2);//pos3-pos2既是域名
+//	string third = url.substr(pos3);//最后一段
+//
+//	cout << first << endl;
+//	cout << second<< endl;
+//	cout << third << endl;
+//	
+//
+//}
+//int main()
+//{
+//	string filename("test.txt.tar.zip");
+//	size_t pos = filename.rfind(".");
+//	string suffix = filename.substr(pos);
+//	cout << suffix << endl;
+//
+//}
+int main()
+{
+	int a = 10000000000000000000000000000000000000;
+	int b = 10000000000000000000000000000000000000;
+	cout << a + b << endl;
+
+}
